730c0e3eadf12ebef23114ac00c94fdfec74aa05
Martin Lauš <79210085+sualkoo@users.noreply.github.com>
2022-05-12 21:18:28 +0200

    Delete zadanie-3-knihy-sualkoo-master directory

--- zadanie-3-knihy-sualkoo-master/src/main/java/sk/stuba/fei/uim/oop/assignment3/authors/data/IAuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package sk.stuba.fei.uim.oop.assignment3.authors.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IAuthorRepository extends JpaRepository<Author, Long> {
-}
